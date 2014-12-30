c965b4b174dc38424a04c55f8d317c6058926c13
Ed Scott <www@gencode.com>
2014-12-30 12:27:05 -0500

    ViewModelBase - Readded as chk in failed last time

---
+++ GenCode.DroneDown/ViewModels/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace GenCode.DroneDown.ViewModels
+{
+	public class BaseViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+		public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			var handler = PropertyChanged;
+			if (handler != null)
+			{
+				handler(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
