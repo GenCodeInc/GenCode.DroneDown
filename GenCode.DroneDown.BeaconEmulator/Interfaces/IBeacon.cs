a1add039c50781305cb1d4d9a0eed17df704c0af
gencodeinc <Orion123>
2014-12-30 12:48:03 -0500

    Added inital emulator service

---
+++ GenCode.DroneDown.BeaconEmulator/Interfaces/IBeacon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace GenCode.DroneDown.BeaconEmulator.Interfaces
+{
+    [ServiceContract]
+    public interface IBeacon
+    {
+        [OperationContract]
+        bool CheckVersion(string version);
+
+        [OperationContract]
+        List<BeaconDevices.BeaconDataPackage> Emulate(BeaconDevices.Manufacturers.XamarinBeaconsLLC beacon);
+    }
+}
---
+++ GenCode.DroneDown.BeaconEmulator/Services/Beacon.svc.cs
@@ -0,0 +1,19 @@
+﻿using GenCode.BeaconDevices.Manufacturers;
+using GenCode.DroneDown.BeaconEmulator.Interfaces;
+
+namespace GenCode.DroneDown.BeaconEmulator.Services
+{
+    public class Beacon : IBeacon
+    {
+        public bool CheckVersion(string version)
+        {
+            throw new System.NotImplementedException();
+        }
+
+     
+        System.Collections.Generic.List<BeaconDevices.BeaconDataPackage> IBeacon.Emulate(XamarinBeaconsLLC beacon)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
